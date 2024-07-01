ecdd6486584ffa14bc8a01ae6b69702f08ab4eea
excf28 <deee4444@gmail.com>
2024-07-01 12:37:33 +0200

    Initial commit

---
+++ combination.js
@@ -0,0 +1,19 @@
+export const combinations = (array, sum) => {
+    const result = [];
+
+    const f = (accumulator, indices, combination) => {
+        let passedIndices = [...indices];
+        for (let index of indices) {
+            passedIndices = passedIndices.filter(i => i !== index);
+            if (accumulator + array[index] === sum) {
+                result.push([...combination, index]);
+            } else if (accumulator + array[index] < sum) {
+                f(accumulator + array[index], passedIndices, [...combination, index])
+            }
+        }
+    }
+
+    f(0, Array.from(array.keys()), []);
+
+    return result;
+}
